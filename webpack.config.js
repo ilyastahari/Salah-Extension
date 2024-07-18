6730e5eea44a5d72071c9374b393aed3e88ba532
Ilyas Tahari <itahari03@gmail.com>
2024-07-18 16:31:58 -0400

    first commit: popup now had content

---
+++ webpack.config.js
@@ -0,0 +1,45 @@
+const path = require('path')
+
+const CopyPlugin = require('copy-webpack-plugin');
+
+const HtmlPlugin = require('html-webpack-plugin')
+
+module.exports = {
+    mode: "development",
+    devtool: 'cheap-module-source-map',
+    entry: {
+        popup: path.resolve('./src/popup/popup.tsx')
+    },
+    module: {
+        rules: [
+            {
+                use: "ts-loader",
+                test: /\.tsx$/,
+                exclude: /node_modules/
+            }
+        ]
+    },
+
+    plugins: [
+        new CopyPlugin({
+            patterns: [
+              { from: path.resolve('src/assets/manifest.json'), 
+              to: path.resolve('dist') },
+
+              { from: path.resolve('src/assets/salah-icon.png'), 
+              to: path.resolve('dist') },
+            ],
+          }),
+        new HtmlPlugin({
+            title: 'Salah Extension',
+            filename: 'popup.html',
+            chunks: ['popup']
+        })
+    ],
+    resolve: {
+        extensions: ['.tsx', '.ts', '.js']
+    },
+    output: {
+        filename: '[name].js'
+    }
+}
